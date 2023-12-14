13803c9eaa83dd633edcbd2dacf26209ecab5b38
rkeerthient <rkeerthi@yext.com>
2023-12-14 18:49:37 +0530

    Updated

--- src/Pages/SearchPage.tsx
+++ src/Pages/SearchPage.tsx
@@ -1,5 +1,14 @@
-import { useSearchActions } from "@yext/search-headless-react";
-import { SearchBar } from "@yext/search-ui-react";
+import {
+  provideHeadless,
+  useSearchActions,
+  VerticalResults as VerticalResultsData,
+} from "@yext/search-headless-react";
+import {
+  DropdownItem,
+  FocusedItemData,
+  RenderEntityPreviews,
+  SearchBar,
+} from "@yext/search-ui-react";
 import { useState } from "react";
 import ProductsGrid from "./ProductsGrid";
 import FAQsPage from "./FAQsPage";
@@ -8,7 +17,9 @@ import ContactInfoPage from "./ContactInfoPage";
 import HomePage from "./HomePage";
 import Locator from "../templates/locator";
 import StoreLocator from "../components/StoreLocator";
-
+import searchConfig from "../components/searchConfig";
+import Ce_device from "../types/devices";
+import { Image } from "@yext/sites-components";
 const SearchPage = () => {
   const searchActions = useSearchActions();
   const [currentPath, setCurrentPath] = useState({
@@ -47,9 +58,97 @@ const SearchPage = () => {
       id: "videos",
     },
   ];
+  const entityPreviewSearcher = provideHeadless({
+    ...searchConfig,
+    headlessId: "entity-preview-searcher",
+  });
+
+  const renderEntityPreviews: RenderEntityPreviews = (
+    autocompleteLoading: boolean,
+    verticalKeyToResults: Record<string, VerticalResultsData>,
+    dropdownItemProps: {
+      onClick: (
+        value: string,
+        _index: number,
+        itemData?: FocusedItemData
+      ) => void;
+      ariaLabel: (value: string) => string;
+    }
+  ): JSX.Element | null => {
+    const productResults = verticalKeyToResults["devices"]?.results.map(
+      (result) => result.rawData
+    ) as unknown as Ce_device[];
+
+    return productResults ? (
+      <div className="grid grid-cols-4 px-2 gap-2 text-black">
+        {productResults.map((result) => (
+          <DropdownItem
+            className="border gap-2"
+            key={result.id}
+            value={result.name}
+            onClick={() => history.pushState(null, "", `/product/${result.id}`)}
+            ariaLabel={dropdownItemProps.ariaLabel}
+          >
+            <DropdownItem
+              key={result.id}
+              value={result.name}
+              ariaLabel={dropdownItemProps.ariaLabel}
+            >
+              <a
+                href={"https://verizon.com"}
+                className="flex flex-col gap-2 w-full"
+              >
+                {result.c_answersPhoto ? (
+                  <Image
+                    image={result.c_answersPhoto}
+                    className="h-full !w-full p-6 mx-auto"
+                  />
+                ) : (
+                  <img
+                    src="https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Femfprotectionaustralia.com.au%2Fwp-content%2Fuploads%2F2015%2F07%2Fplaceholder-product.jpg&f=1&nofb=1&ipt=a9e4d6696ca75f1a86e4956ffe79955d42cd8e7777f0d0d7953c31031fc266b2&ipo=images"
+                    className="h-full !w-full p-6 mx-auto"
+                    alt=""
+                  />
+                )}
+                <div className="flex flex-col gap-2 px-1">
+                  <div className="text-xs">{result.name}</div>
+                  <div className="text-sm">£{result.c_price?.value}</div>
+                </div>
+              </a>
+            </DropdownItem>
+          </DropdownItem>
+        ))}
+      </div>
+    ) : null;
+  };
   return (
     <div className=" w-full px-10 ">
-      <SearchBar />
+      {!currentPath.id ||
+      currentPath.id === "devices" ||
+      currentPath.id === "all" ? (
+        <SearchBar
+          hideRecentSearches={true}
+          customCssClasses={{
+            searchBarContainer: "!mb-0 flex-1 searchBar",
+            searchButton: "text-black",
+          }}
+          visualAutocompleteConfig={{
+            entityPreviewSearcher: entityPreviewSearcher,
+            includedVerticals: ["devices"],
+            renderEntityPreviews: renderEntityPreviews,
+            universalLimit: { devices: 4 },
+            entityPreviewsDebouncingTime: 300,
+          }}
+        />
+      ) : (
+        <SearchBar
+          customCssClasses={{
+            searchBarContainer: "!mb-0 flex-1 searchBar",
+            searchButton: "text-black searchBar",
+          }}
+          hideRecentSearches={true}
+        />
+      )}
       <div className=" bg-white">
         <div className="mx-auto ">
           <div className="h-16 justify-between border-b hidden sm:flex ">
